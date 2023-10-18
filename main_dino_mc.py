e0e0db5db96a1941e431ce74c01bc408bd0a4708
Javier Betancurth Cano <69932736+JavierBetancurth@users.noreply.github.com>
2023-10-17 20:20:53 -0500

    Update main_dino_mc.py

--- main_dino_mc.py
+++ main_dino_mc.py
@@ -159,6 +159,9 @@ def train_dino(args):
     cudnn.benchmark = True
 
     # ============ preparing data ... ============
+    label_map = {"AnnualCrop": 0, "Forest": 1, "HerbaceousVegetation": 2, 
+                 "Highway": 3, "Industrial": 4, "Pasture": 5, "PermanentCrop": 6, "Residential": 7, 
+                 "River": 8, "SeaLake": 9}
     # load dataset
     if args.data_mode == 'mc':
         print('dino-mc')
